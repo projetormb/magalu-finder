e3ee4aa94f8899f90dbd6b5cd922d3256945fdb5
rafael <projetormb@gmail.com>
2017-11-11 20:02:19 -0200

    Ajustes de Layout

--- src/tests.py
+++ src/tests.py
@@ -2,24 +2,20 @@
 
 import unittest2
 
-import json
-
 from apis import api_distancia_google
 
+def test_consumo_google(cep_origem, cep_destino, distanciaEsperada, **kwargs):
 
-# http://127.0.0.1:5000/distancia/?CEPOrigem=13201-031&CEPDestino=14401-216
-# http://127.0.0.1:5000/distancia/?CEPOrigem=14403-587&CEPDestino=14401-216
+    output = '  - CEP de Origem: ' + str(cep_origem) + ' para CEP de Destino ' + str(cep_destino)
 
-def test_consumo_google(cep_origem, cep_destino, distanciaEsperada):
+    if len(kwargs) > 0:
+        for k, v in kwargs.iteritems():
+            if k.lower() == 'description':
+                output += '  (' + str(v) + ')'
 
-    print '  - CEP de Origem: ' + str(cep_origem) + ' para CEP de Destino ' + str(cep_destino)
-
-
-    output = 'OK'
+    print output
 
     ret = api_distancia_google(cep_origem, cep_destino)
-
-
 
     if 'success' in ret:
         if ret['success'] is True:
@@ -28,23 +24,18 @@ def test_consumo_google(cep_origem, cep_destino, distanciaEsperada):
             if 'distance' in ret:
 
                 if 'text' in ret['distance']:
-                    output = '    Distância: ' + str(ret['distance']['text'])
+                    print '  - Distância: ' + str(ret['distance']['text'])
 
                 if 'value' in ret['distance']:
                     distance_value = ret['distance']['value']
 
                     if distance_value == distanciaEsperada:
-                        output += ' - OK'
-                        print output
+                        print '  - OK'
                         print ' '
                         return True
 
 
-
-
-
-
-    print output
+    print '  - retornando False'
     print ' '
 
     return False
@@ -64,6 +55,7 @@ def test_consumoTwitter(userName, maxTweets):
 
 
 class MyTest(unittest2.TestCase):
+
     def test(self):
 
         for x in range(0, 30):
@@ -75,9 +67,17 @@ class MyTest(unittest2.TestCase):
         print '> Testando distância entre 2 CEPs:'
         print ' '
 
-        self.assertEqual(test_consumo_google('13201-031', '14401-216', 347889), True)
-        self.assertEqual(test_consumo_google('13201-031', '14401-216', 347999), False)
-        self.assertEqual(test_consumo_google('14401-216', '69005-140', 0), False)
+        self.assertEqual(test_consumo_google('13201-031', '14401-216', 347889, Description='Jundiai para Franca'), True)
+        self.assertEqual(test_consumo_google('13201-031', '14401-216', 347999, Description='Jundiai para Franca com distancia errada'), False)
+        self.assertEqual(test_consumo_google('69005-140', '14401-216', 3636904, Description='Manaus para Franca'), True)
+        self.assertEqual(test_consumo_google('14401-216', '69005-140', 3623512, Description='Franca para Manaus'), True)
+        self.assertEqual(test_consumo_google('14402-029', '02232-000', 413622, Description='Ceara para Guarulhos'), True)
+
+        self.assertEqual(test_consumo_google('', '14402-029', 0, Description='CEP de Origem invalido'), False)
+        self.assertEqual(test_consumo_google('14402-029', '', 0, Description='CEP de Destino invalido'), False)
+        self.assertEqual(test_consumo_google('69005-140', '14401-216', 0, Description='Distancia nao informada'), False)
+
+
 
         print 'Fim dos testes.'
         print '========================================================================='
